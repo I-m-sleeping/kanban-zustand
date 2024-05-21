8dc726314753e0759c0fb23ec49dea26c44a081a
21120447 <haonhat2729@gmail.com>
2024-05-21 16:15:18 +0700

    use StateCreator to create Slice

--- src/stores/catStore.ts
+++ src/stores/catStore.ts
@@ -1,4 +1,4 @@
-import { create } from "zustand";
+import { StateCreator, create } from "zustand";
 import { immer } from "zustand/middleware/immer";
 import { createSelectors } from "../utils/createSelectors";
 import { devtools, persist, subscribeWithSelector } from "zustand/middleware";
@@ -13,37 +13,47 @@ type TCatStoreState = {
   summary: () => string;
 };
 
+const createCatSlice: StateCreator<
+  TCatStoreState, 
+  [
+    ["zustand/immer", never],
+    ["zustand/devtools", unknown],
+    ["zustand/subscribeWithSelector", never],
+    ["zustand/persist", unknown]
+  ]
+> = (set, get) => ({
+  cats: {
+    bigCats: 0,
+    smallCats: 0
+  },
+  increaseBigCats: ()=>{
+    set(
+      (state) => {
+        state.cats.bigCats++;
+      }
+    )
+  },
+  increaseSmallCats: () => {
+    set(
+      (state) => {
+        state.cats.smallCats++;
+        return state;
+      }
+    )
+  },
+  summary: () => {
+    const total = get().cats.smallCats + get().cats.bigCats;
+    return `There are ${total} cats in total. `;
+  }
+})
+
 export const useCatStore = createSelectors(
   create<TCatStoreState>()(
     immer(
       devtools(
         subscribeWithSelector(
           persist(
-            (set, get) => ({
-              cats: {
-                bigCats: 0,
-                smallCats: 0
-              },
-              increaseBigCats: ()=>{
-                set(
-                  (state) => {
-                    state.cats.bigCats++;
-                  }
-                )
-              },
-              increaseSmallCats: () => {
-                set(
-                  (state) => {
-                    state.cats.smallCats++;
-                    return state;
-                  }
-                )
-              },
-              summary: () => {
-                const total = get().cats.smallCats + get().cats.bigCats;
-                return `There are ${total} cats in total. `;
-              }
-            }),
+            createCatSlice,
             {
               name: "cat store",
             }
