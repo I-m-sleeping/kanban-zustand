cd3867a52f17fc8d2932b88d2f02e09ffe016192
21120447 <haonhat2729@gmail.com>
2024-05-21 16:12:38 +0700

    raise problem

--- src/stores/catStore.ts
+++ src/stores/catStore.ts
@@ -1,6 +1,7 @@
 import { create } from "zustand";
 import { immer } from "zustand/middleware/immer";
 import { createSelectors } from "../utils/createSelectors";
+import { devtools, persist, subscribeWithSelector } from "zustand/middleware";
 
 type TCatStoreState = {
   cats: {
@@ -14,30 +15,43 @@ type TCatStoreState = {
 
 export const useCatStore = createSelectors(
   create<TCatStoreState>()(
-    immer((set, get) => ({
-      cats: {
-        bigCats: 0,
-        smallCats: 0
-      },
-      increaseBigCats: ()=>{
-        set(
-          (state) => {
-            state.cats.bigCats++;
-          }
-        )
-      },
-      increaseSmallCats: () => {
-        set(
-          (state) => {
-            state.cats.smallCats++;
-            return state;
-          }
-        )
-      },
-      summary: () => {
-        const total = get().cats.smallCats + get().cats.bigCats;
-        return `There are ${total} cats in total. `;
-      }
-    })
-  ))  
+    immer(
+      devtools(
+        subscribeWithSelector(
+          persist(
+            (set, get) => ({
+              cats: {
+                bigCats: 0,
+                smallCats: 0
+              },
+              increaseBigCats: ()=>{
+                set(
+                  (state) => {
+                    state.cats.bigCats++;
+                  }
+                )
+              },
+              increaseSmallCats: () => {
+                set(
+                  (state) => {
+                    state.cats.smallCats++;
+                    return state;
+                  }
+                )
+              },
+              summary: () => {
+                const total = get().cats.smallCats + get().cats.bigCats;
+                return `There are ${total} cats in total. `;
+              }
+            }),
+            {
+              name: "cat store",
+            }
+          )
+        ),{
+          name: "cat store",
+        }
+      )
+    )
+  )  
 ) 
