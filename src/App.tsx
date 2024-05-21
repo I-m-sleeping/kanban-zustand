9e2ed7f97ebedebb0f7c375f9e3afafc041c4a5b
21120447 <haonhat2729@gmail.com>
2024-05-21 15:14:12 +0700

    use devtools of redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-import CatBox from "./components/CatBox"
-import CatBox2 from "./components/CatBox2"
-import CatController from "./components/CatController"
+import BearBox from "./components/BearBox"
 
 function App() {
   return (
     <div className="container">
       <h1>Zustand Tutorial</h1>
       <div>
-        <CatBox/>
-        <CatBox2/>
-        <CatController/>
+        <BearBox/>
       </div>
     </div>
   )
