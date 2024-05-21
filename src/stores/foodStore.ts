34926df9c77f9b8609580f5aa58c443324a930c9
21120447 <haonhat2729@gmail.com>
2024-05-21 16:06:37 +0700

    store localStorage

--- src/stores/foodStore.ts
+++ src/stores/foodStore.ts
@@ -1,10 +1,16 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 const initialFoodValue = {
   fish: 0,
+  mouse: 0,
 };
 
-export const useFoodStore = create<typeof initialFoodValue>()(() => initialFoodValue)
+export const useFoodStore = create<typeof initialFoodValue>()(
+  persist(() => initialFoodValue, {
+    name: 'food store',
+  })
+)
 
 export const addOneFish =()=> useFoodStore.setState((state) => ({ fish: state.fish + 1}));
 export const removeOneFish =()=> useFoodStore.setState((state) => ({ fish: state.fish - 1}));
