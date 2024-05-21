5a4fa57b82a265d09c012a948f8e2c034ea545fe
21120447 <haonhat2729@gmail.com>
2024-05-21 15:28:08 +0700

    store localstorage with whitelist

--- src/stores/bearStore.ts
+++ src/stores/bearStore.ts
@@ -1,8 +1,10 @@
 import { create } from "zustand";
-import { createJSONStorage, persist } from "zustand/middleware";
+import { persist } from "zustand/middleware";
 
 type TBearStoreState = {
   bears: number;
+  color: string;
+  size: string;
   increasePopulation: () => void;
   removeAllBears: () => void;
 }
@@ -10,10 +12,12 @@ type TBearStoreState = {
 export const useBearStore = create<TBearStoreState>()(
   persist((set) => ({
     bears: 0,
+    color: "red",
+    size: "big",
     increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
     removeAllBears: () => set ({ bears: 0 }),
   }),{
     name: "bears store",
-    storage: createJSONStorage(() => sessionStorage),
+    partialize: (state) => ({bears: state.bears})
   })
 )
